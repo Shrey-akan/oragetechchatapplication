d5b49660b81ee74cfd947fb1a307b634f3ff48e5
shrey_akan <shereyansjain0904@gmail.com>
2024-01-09 01:52:18 -0800

    update function message to

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function sendMessage(event) {
             content: messageInput.value,
             type: 'CHAT',
             messageFrom: username,
-            messageTo: selectedUser
+            messageTo: selectedUser // Set the selected user as the messageTo
         };
         stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
         messageInput.value = '';
